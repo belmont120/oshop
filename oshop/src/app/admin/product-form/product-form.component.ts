65fb9307827149f88873201f319949e5aa187712
Xin Wang <belmont.xwang@gmail.com>
2018-03-16 10:31:48 +1100

    UpdateProductComplete

--- oshop/src/app/admin/product-form/product-form.component.ts
+++ oshop/src/app/admin/product-form/product-form.component.ts
@@ -12,6 +12,7 @@ import 'rxjs/add/operator/take';
 export class ProductFormComponent implements OnInit {
   categories$;
   product = {};
+  id;
 
   constructor(
     private categoryService: CategoryService,
@@ -23,14 +24,18 @@ export class ProductFormComponent implements OnInit {
   ngOnInit() {
     this.categories$ = this.categoryService.getCategories();
 
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.productService.get(id).take(1).subscribe(p => this.product = p);
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.productService.get(this.id).take(1).subscribe(p => this.product = p);
     }
   }
 
   save(product) {
-    this.productService.create(product);
+    if (this.id) {
+      this.productService.update(this.id, product);
+    } else {
+      this.productService.create(product);
+    }
     this.router.navigate(['/admin/products']);
   }
 }
