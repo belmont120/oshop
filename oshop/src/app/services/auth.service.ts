8c5ed64ff33c60bbc6ce1930e7deaff818f9c693
Xin Wang <belmont.xwang@gmail.com>
2018-03-15 14:50:15 +1100

    AddRedirectionAfterLogin

--- oshop/src/app/services/auth.service.ts
+++ oshop/src/app/services/auth.service.ts
@@ -1,16 +1,19 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class AuthService {
   user$;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
   login() {
+    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
