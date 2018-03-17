45befefdc9b7ac25a9d10e79853c9c9439d35f80
Xin Wang <belmont.xwang@gmail.com>
2018-03-17 15:25:31 +1100

    UpdateShoppingCart

--- oshop/src/app/models/app-shopping-cart.ts
+++ oshop/src/app/models/app-shopping-cart.ts
@@ -6,18 +6,29 @@ export class ShoppingCart {
     constructor(public key: string, public itemsMap: {[productId: string]: ShoppingCartItem }) {
       for (const productId in itemsMap) {
         if ( productId ) {
-          this.items.push(itemsMap[productId]);
+          const item = itemsMap[productId];
+          this.items.push(new ShoppingCartItem(item.product, item.quantity));
         }
       }
     }
 
     get totalItemsCount() {
-        let count = 0;
-        for (const productId in this.items) {
-          if (this.items[productId]) {
-            count += this.items[productId].quantity;
-          }
+      let count = 0;
+      for (const productId in this.items) {
+        if (this.items[productId]) {
+          count += this.items[productId].quantity;
         }
-        return count;
+      }
+      return count;
+    }
+
+    get totalPrice() {
+      let sum = 0;
+      for (const productId in this.items) {
+        if (productId) {
+          sum += this.items[productId].totalPrice;
+        }
+      }
+      return sum;
     }
 }
